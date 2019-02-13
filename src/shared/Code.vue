18262c51e355cc8bc8193d297ffd7a7cac2fcdd3
Vitaliy Stoliarov <huccah@gmail.com>
2019-02-13 22:24:43 +0200

    render Code templates

--- src/shared/Code.vue
+++ src/shared/Code.vue
@@ -5,6 +5,8 @@ pre(v-highlightjs="code")
 
 
 <script>
+import micromustache from 'micromustache';
+
 export default {
   props: {
     lang: {
@@ -14,11 +16,20 @@ export default {
     source: {
       type: String,
       default: null
+    },
+    view: {
+      type: Object,
+      default: () => {}
     }
   },
   data() {
     return {
-      code: '',
+      template: ''
+    }
+  },
+  computed: {
+    code() {
+      return micromustache.render(this.template, this.view);
     }
   },
   methods: {
@@ -33,7 +44,7 @@ export default {
     if(this.$slots.default)
       this.code = this.$slots.default[0].text;
     else if(this.source)
-      this.code = this.getParentCode(this.$parent, this.source);
+      this.template = this.getParentCode(this.$parent, this.source);
     
     // if(!this.code)
       // throw new Error(`Slot or source ${this.source} not found`);
