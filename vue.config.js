0d3c759d5de1827d1b23b240a0ecd9941dccce09
Vitaliy Stoliarov <huccah@gmail.com>
2019-03-09 02:33:15 +0200

    service worker

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,18 @@
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 module.exports = {
     configureWebpack: {
+        plugins: [
+            new WorkboxPlugin.GenerateSW({
+                clientsClaim: true,
+                skipWaiting: true
+            })
+        ],
         module: {
-           rules: [
-             {
-               resourceQuery: /blockType=code/,
-               loader: require.resolve('./loaders/code.js')
-             }
-            ]
+            rules: [{
+                resourceQuery: /blockType=code/,
+                loader: require.resolve('./loaders/code.js')
+            }]
         }
     }
 }
