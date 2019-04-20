51f41a0e9c85f86efd29a5853c06a6b15b7082f7
Vitaliy Stoliarov <huccah@gmail.com>
2019-04-20 23:43:51 +0300

    workbox for prod only

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,15 @@ const path = require('path');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 
+const production = process.env.NODE_ENV === 'production';
+
 module.exports = {
     configureWebpack: {
         plugins: [
-            new WorkboxPlugin.GenerateSW({
+            ...(production ? [new WorkboxPlugin.GenerateSW({
                 clientsClaim: true,
                 skipWaiting: true
-            }),
+            })] : []),
             new WebpackPwaManifest({
                 name: 'Rete.js documentation',
                 short_name: 'Rete.js',
