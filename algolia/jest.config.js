9a11b2ae740e52997cb2a71571ecba1463cf0cf4
Vitaliy Stoliarov <huccah@gmail.com>
2019-04-24 23:46:20 +0300

    add stub

--- algolia/jest.config.js
+++ algolia/jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
     ],
     transform: {
       "^.+\\.js$": "babel-jest",
-      ".*\\.(vue)$": "vue-jest"
+      ".*\\.(vue)$": "vue-jest",
+      ".+\\.(css|styl|less|sass|scss|png|jpg|svg|ttf|woff|woff2|gif)$": "jest-transform-stub"
     },
 };
