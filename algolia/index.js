6a89dd47b34a89d528fd741316fa33f45acd499e
Vitaliy Stoliarov <huccah@gmail.com>
2019-04-25 00:44:09 +0300

    extract text from components and send to algolia

--- algolia/index.js
+++ algolia/index.js
@@ -1,26 +1,33 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils';
+import { mount, createLocalVue } from '@vue/test-utils';
 import algoliasearch from 'algoliasearch';
-import c from '../src/components/Docs/Engine/index';
+import components from './components';
 
-// const client = algoliasearch(
-//     '0S8ITD2OZ2',
-//     '0e372b36fbb8e1c0116c75a3531e36b4'
-//   );
+global.fetch = require('jest-fetch-mock')
+global.fetch.mockResponse(JSON.stringify({ tags: { latest: '0.0.0' }}))
 
-// const localVue = createLocalVue()
-// localVue.directive('t9n', { })
+const client = algoliasearch(
+    '0S8ITD2OZ2',
+    '0e372b36fbb8e1c0116c75a3531e36b4'
+  );
+
+const localVue = createLocalVue()
+localVue.directive('t9n', { })
 
 describe('algolia', () => {
     it('', async () => {
-        const wrapper = shallowMount(c);
-        console.log(wrapper);
+        const index = client.initIndex('common');
+        await index.clearIndex();
 
-        // const index = client.initIndex('common');
+        components.forEach(c => {
+            const wrapper = mount(c, { 
+                localVue,
+                stubs: {
+                    Tooltip: true
+                }
+            });
 
-        // await index.clearIndex();
-        // console.log(wrapper.findAll('p'))
-
-        // index.addObject({ text: wrapper.text() })
+            index.addObject({ text: wrapper.text() })
+        });
     });
 });
   
