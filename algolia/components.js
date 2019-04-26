7998336c03e626faf643631cc4548c3ad6e44301
Vitaliy Stoliarov <huccah@gmail.com>
2019-04-26 17:34:20 +0300

    [algolia] fix path to src (babel dont transpiling files at relative path outside directory with a jest config)

--- algolia/components.js
+++ algolia/components.js
@@ -1,12 +1,12 @@
-import Main from '../src/components/Docs/Main/index.vue'
-import Editor from '../src/components/Docs/Editor/index.vue'
-import Engine from '../src/components/Docs/Engine/index.vue'
-import Components from '../src/components/Docs/Components/index.vue'
-import Nodes from '../src/components/Docs/Nodes/index.vue'
-import Sockets from '../src/components/Docs/Sockets/index.vue'
-import Controls from '../src/components/Docs/Controls/index.vue'
-    // get('../src/components/Docs/Events/index.vue'),
-    // get('../src/components/Docs/Plugins/index.vue')
+import Main from './src/components/Docs/Main/index.vue'
+import Editor from './src/components/Docs/Editor/index.vue'
+import Engine from './src/components/Docs/Engine/index.vue'
+import Components from './src/components/Docs/Components/index.vue'
+import Nodes from './src/components/Docs/Nodes/index.vue'
+import Sockets from './src/components/Docs/Sockets/index.vue'
+import Controls from './src/components/Docs/Controls/index.vue'
+import Events from './src/components/Docs/Events/index.vue'
+// import Plugins from './src/components/Docs/Plugins/index.vue'
 
 export default [
     { component: Main, path: '/docs' },
@@ -15,5 +15,7 @@ export default [
     { component: Components, path: '/docs/components' },
     { component: Nodes, path: '/docs/nodes' },
     { component: Sockets, path: '/docs/sockets' },
-    { component: Controls, path: '/docs/controls' }
+    { component: Controls, path: '/docs/controls' },
+    { component: Events, path: '/docs/events' },
+    // { component: Plugins, path: '/docs/plugins' }
 ]
