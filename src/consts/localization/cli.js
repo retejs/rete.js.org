1b36f612baeebaeccb0785890e6e732f8a2b540f
Vitaliy Stoliarov <huccah@gmail.com>
2019-05-15 23:10:51 +0300

    correct a translation typo

--- src/consts/localization/cli.js
+++ src/consts/localization/cli.js
@@ -11,7 +11,7 @@ export default () => assignSection([
     }],
     ["Сборка вашего пакета должна выполняться с помощью команды (которая уже записана в package.json)", {
         en: "Build your package should be done using the command (which is already wrote in package.json)",
-        ua: "Збірка вашого пакету повинна виконуватись за допомогою команди (яка все записана в package.json)"
+        ua: "Збірка вашого пакету повинна виконуватись за допомогою команди (яка вже записана в package.json)"
     }],
     ["в директории build будут созданы бандлы трех форматов: UMD, CommonJS, ES", {
         en: "In the build directory will be created a bundles of three formats: UMD, CommonJS, ES",
