c0ce8ac1d9278ee6f9998a5ad598841bb5e18e5d
Vitaliy Stoliarov <huccah@gmail.com>
2019-06-04 23:38:37 +0300

    [docs] vue render plugin

--- src/components/Docs/Plugins/list/VueRender.vue
+++ src/components/Docs/Plugins/list/VueRender.vue
@@ -1,7 +1,16 @@
 <template lang="pug">
 .plugin(v-t9n="'docs'")
   Code(source="install")
-  Code(source="components")
+  p Представление контрола основывается на Vue компоненте 
+  Code(source="control_component")
+  p 
+    | В стандартной ситуации кода выше будет достаточно, но 
+    | если вы хотите кастомизировать узлы, вы можете сделать 
+    | это следующим образом. Определить общий Vue компонент
+  Code(source="install_common")
+  p или определить индивидуально для компонента
+  Code(source="node_component")
+  p Работая с динамически изменяемыми узлами или контролами, вы должны выполнять их обновление
   Code(source="use")
   p 
     | В случае, если вы подключаете фреймворк в проекте без Vue.js, вы должны подключить 
@@ -16,24 +25,35 @@ import VueRenderPlugin from 'rete-vue-render-plugin';
 editor.use(VueRenderPlugin);
 </code>
 
-<code name="components">
-import CustomNodeComponent from './CustomNodeComponent.vue';
+<code name="install_common">
+import VueRenderPlugin from 'rete-vue-render-plugin';
+
+editor.use(VueRenderPlugin, {
+  component: CustomNodeComponent
+});
+</code>
+
+<code name="control_component">
 import CustomControlComponent from './CustomControlComponent.vue';
+
+class MyControl extends Rete.Control {
+  constructor(){
+    // ...
+    this.render = 'vue';
+    this.component = CustomControlComponent; // Vue.js компонент, не обязателен
+    this.props = {}; // свойства для компонента выше, не обязателен
+  }
+}
+</code>
+
+<code name="node_component">
+import CustomNodeComponent from './CustomNodeComponent.vue';
 
 class MyComponent extends Rete.Component {
   constructor(){
     // ...
     this.data.render = 'vue';
     this.data.component = CustomNodeComponent; // Vue.js компонент, не обязателен
-    this.data.props = {}; // свойства для компонента выше, не обязателен
-  }
-}
-
-class MyControl extends Rete.Control {
-  constructor(){
-    // ...
-    this.data.render = 'vue';
-    this.data.component = CustomControlComponent; // Vue.js компонент, не обязателен
     this.data.props = {}; // свойства для компонента выше, не обязателен
   }
 }
